e3d62a51c6538ba109a0f02e99d2779de9a21285
Julian0816 <83423059+Julian0816@users.noreply.github.com>
2023-06-24 14:40:37 +0000

    Completed

--- src/app.py
+++ src/app.py
@@ -2,45 +2,29 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for, send_from_directory
+from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
-from api.utils import APIException, generate_sitemap
-from api.models import db
-from api.routes import api
-from api.admin import setup_admin
-from api.commands import setup_commands
-
+from utils import APIException, generate_sitemap
+from admin import setup_admin
+from models import db, Character, Planet, User
 #from models import Person
 
-ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
-static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-# database condiguration
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
-
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-MIGRATE = Migrate(app, db, compare_type = True)
+
+MIGRATE = Migrate(app, db)
 db.init_app(app)
-
-# Allow CORS requests to this API
 CORS(app)
-
-# add the admin
 setup_admin(app)
-
-# add the admin
-setup_commands(app)
-
-# Add all endpoints form the API with a "api" prefix
-app.register_blueprint(api, url_prefix='/api')
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -50,21 +34,160 @@ def handle_invalid_usage(error):
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
-    if ENV == "development":
-        return generate_sitemap(app)
-    return send_from_directory(static_file_dir, 'index.html')
+    return generate_sitemap(app)
 
-# any other endpoint will try to serve it like a static file
-@app.route('/<path:path>', methods=['GET'])
-def serve_any_other_file(path):
-    if not os.path.isfile(os.path.join(static_file_dir, path)):
-        path = 'index.html'
-    response = send_from_directory(static_file_dir, path)
-    response.cache_control.max_age = 0 # avoid cache memory
-    return response
+@app.route('/characters', methods=['GET'])
+def handle_characters():
+
+    characters = Character.query.all()
+    characters_list = list(map(lambda character: character.serialize_all(), characters))
+
+    response_body = {
+        "message": "ok",
+        "results": characters_list
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/characeters/<int:id>', methods=['GET'])
+def handle_character():
+
+    ind_character = Character.query.get(id)
+
+    response_body = {
+        "message": "ok",
+        "results": ind_character.serialize_each()
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/createcharacter', methods=["POST"])
+def create_character():
+    body = json.loads(request.data)
+    new_character = Character(name=body["name"], gender=body["gender"])
+    db.session.add(new_character)
+    db.session.commit()
+
+    return "ok", 200
 
 
-# this only runs if `$ python src/main.py` is executed
+@app.route('/planets', methods=['GET'])
+def handle_planets():
+
+    planets = Planet.query.all()
+    planets_list = list(map(lambda planet: planet.serialize_all(), planets))
+
+    response_body = {
+        "message": "ok",
+        "results": planets_list
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/planets/<int:id>', methods=['GET'])
+def handle_planet():
+
+    ind_planet = Planet.query.get(id)
+
+    response_body = {
+        "message": "ok",
+        "results": ind_planet.serialize_each()
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/createplanet', methods=["POST"])
+def create_planet():
+    body = json.loads(request.data)
+    new_planet = Planet(name=body["name"], population=body["population"], diameter=body["diameter"])
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return "ok", 200
+
+@app.route('/users', methods=['GET'])
+def handle_users():
+        users = User.query.all()
+        users_info =list(map(lambda user: user.serialize_user(), users))
+        response_body = users_info
+        return jsonify(response_body), 200
+
+@app.route('/newuser', methods=["POST"])
+def create_user():
+    body = json.loads(request.data)
+    new_user = User(name=body["name"])
+    db.session.add(new_user)
+    db.session.commit()
+
+    return "ok", 200
+
+@app.route('/user/<int:id>/favorites', methods=['GET'])
+def handling_favoritos(user_id):
+
+    select_character = User.query.filter_by(id=user_id).first().characterFavorite
+    select_planet = User.query.filter_by(id=user_id).first().planetsFavorite
+    Character = list(map(lambda obj: obj.serialize(), select_character))
+    Planets = list(map(lambda obj: obj.serialize(), select_planet))
+
+    return jsonify({
+        "CharacterFavorite": Character,
+        "PlanetsFavorite": Planets
+    }), 200
+
+@app.route('/favorite/character/<int:id>', methods=['POST'])
+def add_character(characterid, userid):
+    user_id = userid
+    user = User.query.get(user_id)
+    character = Character.query.get(characterid)
+    favList = User.query.filter_by(id=user_id).first().characterFavorite
+    favList.append(character)
+    db.session.commit()
+
+    return jsonify({
+        "characterFavorite": list(map(lambda obj: obj.serialize_user(), favList))
+    }), 200
+
+@app.route('/favorite/planet/<int:id>', methods=['POST'])
+def add_planet(planetid, userid):
+    user_id = userid
+    user = User.query.get(user_id)
+    planet = Planet.query.get(planetid)
+    favList = User.query.filter_by(id=user_id).first().planetsFavorite
+    favList.append(planet)
+    db.session.commit()
+
+    return jsonify({
+        "planetsFavorite": list(map(lambda obj: obj.serialize_user(), favList))
+    }), 200
+
+@app.route('/favorite/character/<int:id>', methods=['DELETE'])
+def removeFavCharacter(characterid, userid):
+    user_id = userid
+    user = User.query.get(user_id)
+    character = Character.query.get(characterid)
+    favList = User.query.filter_by(id=user_id).first().characterFavorite
+    favList.remove(character)
+    db.session.commit()
+
+    return jsonify({
+        "characterFavorite": list(map(lambda obj: obj.serialize_user(), favList))
+    }), 200
+
+@app.route('/favorite/character/<int:id>', methods=['DELETE'])
+def removeFavPlanet(planetid, userid):
+    user_id = userid
+    user = User.query.get(user_id)
+    planet = Planet.query.get(planetid)
+    favList = User.query.filter_by(id=user_id).first().planetsFavorite
+    favList.remove(planet)
+    db.session.commit()
+
+    return jsonify({
+        "planetsFavorite": list(map(lambda obj: obj.serialize_user(), favList))
+    }), 200
+
+
+# this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3001))
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+    PORT = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=PORT, debug=False)
